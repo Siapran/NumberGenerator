e442127811bb8ac5898a315078fd903877c53282
siapran <siapran.candoris@gmail.com>
2015-11-11 17:47:02 +0100

    Added foreach macro.

--- entiers.c
+++ entiers.c
@@ -24,7 +24,16 @@ List_Node *List_New_node(List_Node *prev, Node *node) {
 	return self;
 }
 
+void List_Delete(List_Node *self) {
+	List_Node *old_self;
+	while (self != NULL) {
+		old_self = self;
+		self = self->next;
+		free(old_self);
+	}
+}
 
+#define foreach(head, root) for (head = root; head != NULL; head = head->next)
 
 void read_input() {
 	size_t i;
@@ -47,16 +56,39 @@ int main()
 }
 
 List_Node *getOpTrees(Vector *values) {
-	
-	List_Node *list = NULL;
-	size_t index, i;
-	Vector *leftlist, *rightlist;
+
+	List_Node
+	*root, *head,
+	*leftTree_root, *leftTree_head,
+	*rightTree_root, *rightTree_head,;
+
+	Node *node;
+	Operator op;
+
+	size_t index, i, j;
+	Vector leftlist, rightlist;
+
 	if (values.size == 1)
 	{
 		return List_New_node(NULL, IntNode_New(values->data[0]));
 	}
+
 	for (index = 1; index < values->size; ++index) {
-		
+		leftlist = Vector_Get_Subvector(values, 0, index);
+		rightlist = Vector_Get_Subvector(values, index, values->size);
+
+		leftTree_root = getOpTrees(leftlist);
+		rightTree_root = getOpTrees(rightlist);
+
+		foreach (leftTree_head, leftTree_root) {
+			foreach (rightTree_head, rightTree_root) {
+				for (op = PLUS; op <= DIVIDE; ++op) {
+					node = OperatorNode_New(leftTree_head->node, op, rightTree_head->node);
+				}
+			}
+		}
+
+		List_Delete(leftTree_root);
+		List_Delete(rightTree_root);
 	}
 }
-
